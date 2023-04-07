850d75a74ec4b99310cf5b8cba5f4b0ebea8efe4
Scott Blatt <scott.blatt.0b10@gmail.com>
2023-04-07 11:19:35 -0500

    Make bootstrap scipts better

--- instances/vanilla/bootstrap.sh
+++ instances/vanilla/bootstrap.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
+
+PREFIX="$(cd -- "$(dirname "$0")"; pwd -P)"
+
 mkdir data
+
+cat <<EOF > overrides.yml
+services:
+  mc:
+    environment:
+      MOTD: "Pixel's MC Server\\\\nCurrent Modpack: None"
+      VERSION: LATEST
+
+volumes:
+  data:
+    driver: local
+    driver_opts:
+      type: 'none'
+      o: 'bind'
+      device: './instances/vanilla/data'
+EOF
