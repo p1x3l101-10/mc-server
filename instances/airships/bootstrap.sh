f5007658f51c98680f7edabeedb6381929bb1507
pixel <scott.blatt.0b10@gmail.com>
2023-04-27 23:47:39 -0500

    Change MOTD

--- instances/airships/bootstrap.sh
+++ instances/airships/bootstrap.sh
@@ -26,7 +26,7 @@ services:
   mc:
     image: itzg/minecraft-server:java17
     environment:
-      MOTD: "Pixel's MC Server\\\\nCurrent Modpack: None"
+      MOTD: "Pixel's MC Server\\\\nCurrent Modpack: Airships"
       VERSION: 1.19.4
       TYPE: FABRIC
       MODRINTH_PROJECTS: '$MODRINTH_MODS_INTERNAL'
