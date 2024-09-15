d5939ff7a8222f0afef6dbe36d4b647363e0e792
Pixel <scott.blatt.0b10@gmail.com>
2024-09-14 19:20:52 -0500

    stop early on new container

--- run.sh
+++ run.sh
@@ -16,6 +16,10 @@ if ! [ -d "$PREFIX" ]; then
     echo "EULA=false" >> "${PREFIX}/minecraft.env"
     echo "docker.io/itzg/minecraft-server:latest" > "${PREFIX}/config/runtime"
     echo "25565" > "${PREFIX}/config/port"
+
+    echo "Skeleton config has been setup, make sure to configure it"
+    echo "BE SURE TO AGREE TO THE EULA, OR THE CONTAINER WILL FAIL!!!"
+    exit 1
 fi
 
 exec podman run --name="$NAME" --env-file="${PREFIX}/minecraft.env" --volume="${PREFIX}/data:/data:rw" --publish=25565:$(cat "${PREFIX}/config/port") $(cat "${PREFIX}/config/runtime")
