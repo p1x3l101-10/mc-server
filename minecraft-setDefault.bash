ee2b0721b1b315ba3b495132e618aba966a8bf43
Pixel <scott.blatt.0b10@gmail.com>
2024-10-28 14:30:38 -0500

    default target

--- minecraft-setDefault.bash
+++ minecraft-setDefault.bash
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 
 # Root check
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+if [[ "$EUID" -ne 0 ]]; then
+  echo "Please run as root"
   exit 1
 fi
 
 # Script start
 INSTANCE="${1}"
 
-cat <<EOF > /etc/systemd/system/minecraft-default.service
+cat <<EOF > /etc/systemd/system/minecraft.target
 [Unit]
 Description=Shim for minecraft@${INSTANCE}.service
+AllowIsolate=no
 Requires=minecraft@${INSTANCE}.service
 
 [Install]
 WantedBy=default.target
 EOF
-
 systemctl daemon-reload
+
+# Restart if already running
+[[ "$(systemctl show -property=ActiveState --value=minecraft.target)" == "running" ]] && systemctl restart minecraft.target
