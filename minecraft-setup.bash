347218a49d3c356a0084632819bd0e540152b7a4
Pixel <scott.blatt.0b10@gmail.com>
2024-10-28 13:36:21 -0500

    actually generate the starter config file

--- minecraft-setup.bash
+++ minecraft-setup.bash
@@ -2,14 +2,15 @@
 NAME="$1"
 PREFIX="/var/lib/minecraft/${NAME}"
 
-if ! [ -d "$PREFIX" ]; then
-    if [ -e "$PREFIX" ]; then
+if ! [[ -d "${PREFIX}" ]]; then
+    if [[ -e "${PREFIX}" ]]; then
         echo "Prefix already exists, and is not a directory"
         exit 1
     fi
     echo "Creating prefix for '${NAME}'"
-    mkdir -p "$PREFIX"
-    mkdir "$PREFIX/data"
+    mkdir -p "${PREFIX}"
+    mkdir "${PREFIX}/data"
+    echo -e "# See documentation at 'https://docker-minecraft-server.readthedocs.io/en/latest/variables'\nEULA=false" > "${PREFIX}/config.env"
     echo "Prefix for '${NAME}' created, please configure it at '${PREFIX}/config.env'"
     exit 1
 fi
